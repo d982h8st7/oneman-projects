88e3dfefc6e88574a2cd8544bf271bcb820f1843
d982h8st7 <sinnarver15@gmail.com>
2023-05-16 19:45:18 +0900

    upload step-2's .png file and attach png and additional ontents, title. need to find how to refresh about uploading files

--- testing_with_debugged_chrome.py
+++ testing_with_debugged_chrome.py
@@ -11,57 +11,27 @@ import time
 
 options = Options()
 options.add_experimental_option('debuggerAddress', '127.0.0.1:9222')
-driver = webdriver.Chrome(service=Service('D:\\task\\py\\1\\chromedriver.exe'), options=options)
-
-
-
-data = pd.read_excel('yami.xlsx')  # 엑셀파일 저장
-# 조식, 중식, 석식 구분값 추출
-values1 = data.iloc[1, 0]  # 첫 번째 열('A'열)의 3행부터 7행까지 값
-values2 = data.iloc[14, 0]  # 첫 번째 열('A'열)의 18행부터 20행까지 값
-values3 = data.iloc[18, 0]  # 첫 번째 열('A'열)의 22행부터 26행까지 값
-# values를 문자열로 변환하고, 각 값 사이에 줄 바꿈을 추가합니다.
-values_str = values1
-values_str1 = values2
-values_str2 = values3
-
-# 메모장에 저장합니다.
-output_file_path = r'D:\for_project_files\output.txt'
-with open(output_file_path, 'w') as f:
-    f.write(values_str + '\n\n\n\n' + '\n\n\n\n' + values_str1 + '\n\n\n' + '\n\n\n' + values_str2)
-
-menu_sec1 = data.iloc[1:7, 2].astype(str)  # 첫 번째 열('A'열)의 3행부터 7행까지 값
-menu_sec2 = data.iloc[14:18, 2].astype(str)  # 첫 번째 열('A'열)의 18행부터 20행까지 값
-menu_sec3 = data.iloc[18:24, 2].astype(str)  # 첫 번째 열('A'열)의 22행부터 26행까지 값
-# values를 문자열로 변환하고, 각 값 사이에 줄 바꿈을 추가합니다.
-menu_val1 = ' \n'.join(menu_sec1.values)+'ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ'
-menu_val2 = " \n".join(menu_sec2.values)+'ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ'
-menu_val3 = " \n".join(menu_sec3.values)+'ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ'
-
-
-# # 메모장에 저장합니다.
-output_file_path = r'D:\for_project_files\output2.txt'
-with open(output_file_path, 'w') as f:
-    f.write(menu_val1 + '\n\n' + menu_val2 + '\n\n' + menu_val3)
-
-a = datetime.datetime.now()
+driver = webdriver.Chrome(service=Service('./chromedriver.exe'), options=options)
 
 
 
 try:
-    driver.get('https://everytime.kr/393849/')
+    # driver.get('https://everytime.kr/393849/')
     # time.sleep(40)
-    driver.find_element(By.XPATH,'//a[@id="writeArticleButton"]').click()
+    # driver.find_element(By.XPATH,'//a[@id="writeArticleButton"]').click()
+    #upload_button = driver.find_element(By.CLASS_NAME, 'attach')
     title = driver.find_element(By.CSS_SELECTOR, 'input.title')
     content = driver.find_element(By.XPATH,'//textarea[@class="smallplaceholder"]')
-    main_contents = values_str + menu_val1 + '\n' + values_str1 + menu_val2 + '\n' + values_str2 + menu_val3 + '\n\n\n\n'
 
-    additional_content = "crawling test with connecting everytime.kr with selenium.\n" \
-                         "You can see the code source in https://github.com/d982h8st7/oneman-projects/tree/main/1\n"+a
-    # 두 문자열을 결합
+    #upload_button.send_keys('C:\\Users\\User\\Desktop\\파이썬\\workingArea\\Project\\files\\5_17_menu.png')
+    additional_content = "학교사이트에서 학식 엑셀파일을 크롤링, 데이터를 추출하여 사진으로 변환, 에타에 자동으로 올리는 파이썬 프로젝트입니다.\n" \
+                         "학식자동알림ver1.2 코드의 원본은 https://github.com/d982h8st7/oneman-projects/tree/main/1 에서 보실수있습니다. \n" \
+                         "메뉴 데이터의 원본 컨텐츠의 저작권 귀속은 영남이공대학교에 있으며, 본 코드를 인용, 개조하여 불이익이 발생시 코드제작자는 어떠한 책임도 지지않습니다. \n" \
+                         "글 작성시각 :2023-05-16 19:35:23.170447"
+    # 두 문자열을 결합     "
     title.send_keys('2023년 5월 16일 학식 메뉴')  # 실제 제목으로 변경해야 함
-    content.send_keys(main_contents+additional_content)
-
+    content.send_keys(additional_content)
+    time.sleep(10)
     driver.find_element(By.XPATH,'//li[@class="submit"]').click()  # 실제 게시글 작성 버튼의 셀렉터로 변경해야 함
 except Exception as e:
     print(str(e))
